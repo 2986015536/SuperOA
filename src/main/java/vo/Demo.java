99adc17ace86eb8825076310189b20c4bb64b092
2986015536 <2986015536@qq.com>
2021-08-16 17:50:33 +0800

    第二次修改

--- src/main/java/vo/Demo.java
+++ src/main/java/vo/Demo.java
@@ -1,10 +1,12 @@
 package vo;
 
-public class Demo<mian> {
+public class Demo {
     public static void main(String[] args) {
         System.out.println("我是个帅逼");
 
         System.out.println("我是个帅逼");
+
+        System.out.println("asdasdasdasd");
 
         System.out.println("asdasdasdasd");
     }
